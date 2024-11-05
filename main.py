77f5960d2251453f3a65d81dd7c50018f4231323
Tharinivarsha <chitraswamy11@gmail.com>
2024-11-05 17:34:01 +0530

    Power Set

---
+++ main.py
@@ -0,0 +1,19 @@
+def printPowerSet(set, set_size):
+    power_set_size = 2**set_size
+    outer = 0
+    inner = 0
+    for outer in range(0, power_set_size):
+        for inner in range(0,set_size):
+            if((outer& (1<<inner))>0):
+                print(set[inner],end="")
+        print("")
+
+size=int(input("enter size of array"))
+
+set=[]
+for i in range (0,size):
+    n=int(input("enter elements"))
+    set.append(n)
+
+printPowerSet(set,len(set))
+  
